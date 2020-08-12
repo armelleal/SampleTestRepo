4ba41659c9e1cd53a153d1260956da9339dd2465
Armel S Leal <armelsleal@Salvadors-MacBook-Pro.local>
2020-08-12 15:44:18 -0500

    added changes to feature branch

--- SampleTestproject/Program.cs
+++ SampleTestproject/Program.cs
@@ -7,6 +7,19 @@ namespace SampleTestproject
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine($"This is an even number {i}");
+                    cw
+                }
+                else
+                {
+                    Console.WriteLine($"This is an odd nuber {i}");
+                }
+            }
         }
     }
 }
