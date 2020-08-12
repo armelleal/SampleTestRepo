7dbb7c60abaa6f1fb8db0ce07762e2536017ebaa
Armel S Leal <armelsleal@Salvadors-MacBook-Pro.local>
2020-08-12 15:15:54 -0500

    initial commit

---
+++ SampleTestproject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SampleTestproject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
